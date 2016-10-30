0cb1ee8ba7db68e48e3bf9e23f2cdc0f89291d5c
brunosimon <simon.bruno.77@gmail.com>
2016-10-30 20:56:19 +0100

    Update responsive lesson

--- exercices/cours-05-audio-video-g1/script.js
+++ exercices/cours-05-audio-video-g1/script.js
@@ -1,16 +1,58 @@
+// Create all needed variables
 var player = {};
 
-player.elements             = {};
-player.elements.container   = document.querySelector( '.player' );
-player.elements.video       = player.elements.container.querySelector( 'video' );
-player.elements.toggle_play = player.elements.container.querySelector( '.toggle-play' );
+player.elements               = {};
+player.elements.container     = document.querySelector( '.player' );
+player.elements.video         = player.elements.container.querySelector( 'video' );
+player.elements.controls      = player.elements.container.querySelector( '.controls' );
+player.elements.toggle_play   = player.elements.controls.querySelector( '.toggle-play' );
+player.elements.seek_bar      = player.elements.controls.querySelector( '.seek-bar' );
+player.elements.seek_bar_fill = player.elements.seek_bar.querySelector( '.fill' );
 
-player.elements.toggle_play.addEventListener( 'click', function( event )
-{
+// Listen to click on toggle play button
+player.elements.toggle_play.addEventListener( 'click', function( event ){
+
+	// Toggle play
 	if( player.elements.video.paused )
 		player.elements.video.play();
 	else
 		player.elements.video.pause();
 
+	// Prevent default event
 	event.preventDefault();
 } );
+
+// Video play event
+player.elements.video.addEventListener( 'play', function(){
+
+	// Change container class
+	player.elements.container.classList.add( 'playing' );
+} );
+
+// Video pause event
+player.elements.video.addEventListener( 'pause', function(){
+
+	// Change container class
+	player.elements.container.classList.remove( 'playing' );
+} );
+
+// Seekbar click event
+player.elements.seek_bar.addEventListener( 'click', function( event ){
+
+	// Get ratio between 0 and 1
+	var ratio = (event.clientX - player.elements.seek_bar.offsetLeft);
+	ratio /= player.elements.seek_bar.offsetWidth;
+
+	// Get time according to ratio
+	var time = ratio * player.elements.video.duration;
+
+	// Update time
+	player.elements.video.currentTime = time;
+} );
+
+window.setInterval( function()
+{
+	var ratio = player.elements.video.currentTime / player.elements.video.duration;
+	
+	player.elements.seek_bar_fill.style.transform = 'scaleX(' + ratio + ')';
+}, 50 );
--- exercices/cours-05-audio-video-g2/script.js
+++ exercices/cours-05-audio-video-g2/script.js
@@ -1,16 +1,61 @@
 var player = {};
 
-player.el             = {};
-player.el.container   = document.querySelector( '.player' );
-player.el.video       = player.el.container.querySelector( 'video' );
-player.el.toggle_play = player.el.container.querySelector( 'a.toggle-play' );
+// Create every needed variables
+player.el               = {};
+player.el.container     = document.querySelector( '.player' );
+player.el.video         = player.el.container.querySelector( 'video' );
+player.el.controls      = player.el.container.querySelector( '.controls' );
+player.el.toggle_play   = player.el.controls.querySelector( 'a.toggle-play' );
+player.el.seek_bar      = player.el.controls.querySelector( '.seek-bar' );
+player.el.seek_bar_full = player.el.seek_bar.querySelector( '.full' );
 
-player.el.toggle_play.addEventListener( 'click', function( event )
-{
+
+// Listen to click event on toggle play button
+player.el.toggle_play.addEventListener( 'click', function( event ){
+
+	// Toggle play
 	if( player.el.video.paused )
 		player.el.video.play();
 	else
 		player.el.video.pause();
 
+	// Prevent default event
 	event.preventDefault();
 } );
+
+// Listen to play event on video
+player.el.video.addEventListener( 'play', function(){
+
+	// Update class
+	player.el.container.classList.add( 'playing' );
+} );
+
+// Listen to pause event on video
+player.el.video.addEventListener( 'pause', function(){
+
+	// Update class
+	player.el.container.classList.remove( 'playing' );
+} );
+
+// Listen to click event on seek bar
+player.el.seek_bar.addEventListener( 'click', function( event ){
+
+	// Calculate the time according to the mouse position
+	var seek_bar_width = player.el.seek_bar.offsetWidth,
+		seek_bar_left  = player.el.seek_bar.offsetLeft,
+		mouse_x        = event.clientX,
+		ratio          = ( mouse_x - seek_bar_left ) / seek_bar_width,
+		time           = ratio * player.el.video.duration;
+
+	player.el.video.currentTime = time;
+
+	event.preventDefault();
+} );
+
+window.setInterval( function(){
+	var duration = player.el.video.duration,
+		time     = player.el.video.currentTime,
+		ratio    = time / duration;
+
+	player.el.seek_bar_full.style.transform = 'scaleX(' + ratio + ')';
+}, 50 );
