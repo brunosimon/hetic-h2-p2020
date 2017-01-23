abd7150d1debfbb823725d4ff90b591754974e19
brunosimon <simon.bruno.77@gmail.com>
2017-01-23 13:57:11 +0100

    Update PHP debug lesson

---
+++ exercices/cours-14-php-les-bases-g1/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mon site</title>
+</head>
+<body>
+	<header>
+		<?php echo $title; ?>
+	</header>
--- exercices/cours-14-php-les-bases-g1/hello-world.php
+++ exercices/cours-14-php-les-bases-g1/hello-world.php
@@ -1,14 +1,6 @@
 <?php
-	
-    $news = [
-    	'article 1',
-    	'article 2',
-    	'article 3',
-    	'article 4',
-    	'article 5',
-	];
 
-	foreach($news as $_key => $_news)
-	{
-		echo '<br>'.$_key.' = '.$_news;
-	}
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+	phpinfo();
